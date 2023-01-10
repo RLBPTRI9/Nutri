d2d3efa20a18e8d3b18a1563513ab8f3a5193323
Matthew MacDonald <matthewmacdonald819@gmail.com>
2023-01-09 21:06:07 -0800

    completed api fetch and logic with result object. Able to create list of recipes without allergy but still need some work on allergy search.

--- server/controllers/inputController.js
+++ server/controllers/inputController.js
@@ -6,9 +6,10 @@ const inputController = {};
 
 inputController.getIngredients = (req, res, next) => {
   //pad thai, pad+thai
-  //const  = req.params.ingredient;
+  const { ingredient, allergy } = req.params;
 
   //clean up user input - trim spaces + lower cases
+
   console.log(process.env.EDAMAM_RECIPE_API_ID);
 
   const url = `https://api.edamam.com/search?app_id=${process.env.EDAMAM_RECIPE_API_ID}&app_key=${process.env.EDAMAM_RECIPE_API_KEY}&q=pad+thai`;
@@ -18,43 +19,69 @@ inputController.getIngredients = (req, res, next) => {
       if (!response.ok) {
         throw new Error('Not 2xx response', { message: response });
       }
-      console.log(response);
+      // console.log(response);
       return response.json();
     })
     .then((data) => {
-      console.log(data);
-      res.locals.ingredientData = data;
+      // console.log(data);
+      // console.log(data.hits);
+      const recipes = data.hits; //array of objects, each object containing
+
+      // let counter = 0;
+      // let totalRecipes = recipes.length;
+      // let allergyCheck = false;
+      let resultList = [];
+
+      //we will need allergy from front end or need to query database
+
+      recipes.forEach((ele) => {
+        const x = {};
+        x.name = ele.recipe.label;
+        x.image = ele.recipe.image;
+        x.url = ele.recipe.url;
+        x.ingredients = ele.recipe.ingredientLines;
+        x.cautions = ele.recipe.cautions;
+        x.healthLabels = ele.recipe.healthLabels;
+        // console.log(x);
+        resultList.push(x);
+        // console.log(resultList);
+      });
+
+      const allergy = 'peanut';
+
+      //const allergies = localStorage.getItem('allergies');
+      //console.log(allergies);
+
+      const recipesWithAllergy = [];
+      // const recipesWithoutAllergy = [];
+
+      const withoutAllergy = [];
+      for (let recipe of resultList) {
+        let ingredients = recipe.ingredients;
+        // console.log(ingredients);
+        for (let ingredient of ingredients) {
+          // console.log('ingredient',ingredient)
+          if (ingredient.match(allergy)) {
+            recipesWithAllergy.push(recipe.url);
+            break;
+          }
+        }
+      }
+      for (let ele of resultList) {
+        if (!recipesWithAllergy.includes(ele.url)) {
+          withoutAllergy.push(ele);
+        }
+      }
+      console.log(withoutAllergy);
+      res.locals.without = withoutAllergy; //food recipes list not containing allergy sent back to front end
+
+      // res.locals.totalRecipes = totalRecipes; //number of recipes searched
+      // res.locals.recipesContainingAllergy = counter; //number of recipes searched that containin allergy
       return next();
     })
     .catch((err) => {
       if (err) return next(console.log(err));
     });
 };
-
-// swapiController.getMoreCharacterData = (req, res, next) => {
-
-//     // const characterID = req.params.id;
-//     const {id} = req.params;
-
-//     const url = `https://swapi.dev/api/people/${id}`;
-//     const charId = req.params.id;
-//     fetch(url)
-//       .then(response => {
-//         if (!response.ok) {
-//           throw new Error('Not 2xx response', ({message: response}));
-//         }
-//         response.json();
-//       })
-//       .then(json => {
-//         res.locals.characterData = json;
-//         return next();
-//       })
-//       .catch((err) => {
-//         if (err) return next(createErr({
-//           method: 'Get more character data',
-//           type: 'fetching character data via swapi',
-//           err,
-//         }));
-//       });
 
 module.exports = inputController;
