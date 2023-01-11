b54f229729c042be44b2250f0894e46842506c94
michellesylee <michellebellelee@yahoo.com>
2023-01-10 18:06:02 -0800

    started grid

--- client/components/RecipeCards.jsx
+++ client/components/RecipeCards.jsx
@@ -15,6 +15,42 @@ import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 
+
+
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+// function BasicGrid() {
+//   return (
+//     <Box sx={{ flexGrow: 1 }}>
+//       <Grid container spacing={2}>
+//         <Grid item xs={8}>
+//           <Item>xs=8</Item>
+//         </Grid>
+//         <Grid item xs={4}>
+//           <Item>xs=4</Item>
+//         </Grid>
+//         <Grid item xs={4}>
+//           <Item>xs=4</Item>
+//         </Grid>
+//         <Grid item xs={8}>
+//           <Item>xs=8</Item>
+//         </Grid>
+//       </Grid>
+//     </Box>
+//   );
+// }
+
+
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
 
@@ -37,6 +73,27 @@ export default function RecipeReviewCard({ props }) {
 
   return (
     <div className="viewCard">
+
+
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <Item>xs=6</Item>
+        </Grid>
+        <Grid item xs={6}>
+          <Item>xs=6</Item>
+        </Grid>
+        <Grid item xs={6}>
+          <Item>xs=6</Item>
+        </Grid>
+        <Grid item xs={6}>
+          <Item>xs=6</Item>
+        </Grid>
+      </Grid>
+    </Box>
+
+
+
       <Card sx={{ maxWidth: 500 }}>
         <CardHeader
           avatar={
