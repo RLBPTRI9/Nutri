233e0b3ba543456216eef870633d21b1fad7cdfe
Martin Carmona <martin.e.carmona@gmail.com>
2023-01-11 15:32:34 -0800

    add more styling to components

--- client/components/RecipeCard.jsx
+++ client/components/RecipeCard.jsx
@@ -97,63 +97,59 @@ export default function RecipeReviewCard({ props }) {
       <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={2}>
           <Grid item xs={6}>
-            <Item>
-              {/* <Card sx={{ maxWidth: 500 }}> */}
-
-              <Card sx={{ maxWidth: 500 }}>
-                <CardHeader
-                  avatar={
-                    <Avatar sx={{ bgcolor: red[500] }} aria-label='recipe'>
-                      R
-                    </Avatar>
-                  }
-                  action={
-                    <IconButton aria-label='settings'>
-                      <MoreVertIcon />
-                    </IconButton>
-                  }
-                  //  title="Shrimp and Chorizo Paella"
-                  title={name}
-                  subheader='September 14, 2016'
-                />
-                <CardMedia
-                  component='img'
-                  height='280'
-                  image={image}
-                  alt='Paella dish'
-                />
+            <Card sx={{ maxWidth: 500 }} elevation={12}>
+              <CardHeader
+                avatar={
+                  <Avatar sx={{ bgcolor: red[500] }} aria-label='recipe'>
+                    R
+                  </Avatar>
+                }
+                action={
+                  <IconButton aria-label='settings'>
+                    <MoreVertIcon />
+                  </IconButton>
+                }
+                //  title="Shrimp and Chorizo Paella"
+                title={name}
+                subheader='September 14, 2016'
+              />
+              <CardMedia
+                component='img'
+                height='280'
+                image={image}
+                alt='Paella dish'
+              />
+              <CardContent>
+                <Typography variant='body2' color='text.secondary'>
+                  Tags: {healthLabels}
+                  <br />
+                  <br />
+                  Click <a href={url}> link</a> for recipe
+                </Typography>
+              </CardContent>
+              <CardActions disableSpacing>
+                <IconButton aria-label='add to favorites'>
+                  <FavoriteIcon />
+                </IconButton>
+                <IconButton aria-label='share'>
+                  <ShareIcon />
+                </IconButton>
+                <ExpandMore
+                  expand={expanded}
+                  onClick={handleExpandClick}
+                  aria-expanded={expanded}
+                  aria-label='show more'
+                >
+                  <ExpandMoreIcon />
+                </ExpandMore>
+              </CardActions>
+              <Collapse in={expanded} timeout='auto' unmountOnExit>
                 <CardContent>
-                  <Typography variant='body2' color='text.secondary'>
-                    Tags: {healthLabels}
-                    <br />
-                    <br />
-                    Click <a href={url}> link</a> for recipe
-                  </Typography>
+                  <Typography paragraph>Ingredients:</Typography>
+                  <Typography paragraph>{ingredients}</Typography>
                 </CardContent>
-                <CardActions disableSpacing>
-                  <IconButton aria-label='add to favorites'>
-                    <FavoriteIcon />
-                  </IconButton>
-                  <IconButton aria-label='share'>
-                    <ShareIcon />
-                  </IconButton>
-                  <ExpandMore
-                    expand={expanded}
-                    onClick={handleExpandClick}
-                    aria-expanded={expanded}
-                    aria-label='show more'
-                  >
-                    <ExpandMoreIcon />
-                  </ExpandMore>
-                </CardActions>
-                <Collapse in={expanded} timeout='auto' unmountOnExit>
-                  <CardContent>
-                    <Typography paragraph>Ingredients:</Typography>
-                    <Typography paragraph>{ingredients}</Typography>
-                  </CardContent>
-                </Collapse>
-              </Card>
-            </Item>
+              </Collapse>
+            </Card>
           </Grid>
         </Grid>
       </Box>
