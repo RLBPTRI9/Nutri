488dfbc40982f9bb19fe2065117cd21b7f339cfd
Martin Carmona <martin.e.carmona@gmail.com>
2023-01-10 21:23:39 -0800

    add dark mode

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,14 +3,30 @@ import GetIngredients from './GetIngredients.jsx';
 import AddAllergy from './AddAllergy.jsx';
 import '../stylesheets/styles.css';
 import ResponsiveAppBar from './NavBar.jsx';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode]
+  );
+
   return (
-    <div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <ResponsiveAppBar />
       <AddAllergy />
       <GetIngredients />
-    </div>
+    </ThemeProvider>
   );
 }
 
