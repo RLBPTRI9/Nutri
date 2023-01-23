c4e223fbe5a4a286b90170b4226dab30982072c0
Mathew Hultquist <mathew@gates.services>
2023-01-23 11:19:21 -0500

    Added basic boilerplate

---
+++ backend/middleware/favorites.ts
@@ -0,0 +1,3 @@
+const favorites = {};
+
+export default favorites;
---
+++ backend/routes/favorites.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+const route = Router();
+
+route.get('/', () => {});
+route.post('/', () => {});
+route.patch('/', () => {});
+route.delete('/', () => {});
+
+export default route;
