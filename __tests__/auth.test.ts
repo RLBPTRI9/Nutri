1fd733953e82106f226064428f71ed101c515f21
Mathew Hultquist <mathew@gates.services>
2023-01-24 13:56:03 -0500

    Added signup and get user tests

--- __tests__/auth.test.ts
+++ __tests__/auth.test.ts
@@ -5,7 +5,7 @@ import UserModel from '../backend/models/UserModel';
 import { testUser } from './utils/testData';
 import request from 'supertest';
 import app from '../backend/server';
-import { formatCookie } from './utils/functions';
+import { formatCookie, strip } from './utils/functions';
 env.config({ path: '../.env' });
 
 const DB_URI: string = process.env.MONGO_URI!;
@@ -15,12 +15,13 @@ const testApp = request(app);
 
 describe('auth', () => {
   describe('login', () => {
-    const expiration = Date.now() + 864000000;
+    let expiration: number | undefined;
     let user: User | undefined;
     let db: typeof mongoose | undefined;
     beforeEach(async () => {
       db = await mongoose.connect(DB_URI);
       user = await UserModel.create(testUser);
+      expiration = Date.now() + 864000000;
     });
 
     afterEach(async () => {
@@ -28,6 +29,7 @@ describe('auth', () => {
       user = undefined;
       await db?.disconnect();
       db = undefined;
+      expiration = undefined;
     });
 
     it('should respond with the user', async () => {
@@ -70,7 +72,7 @@ describe('auth', () => {
       expect(formattedCookie.httponly).toBe(true);
       expect(
         new Date(formattedCookie.expires).getMilliseconds()
-      ).toBeGreaterThanOrEqual(expiration);
+      ).toBeGreaterThanOrEqual(expiration!);
       expect(res.status).toBe(200);
     });
 
@@ -127,15 +129,173 @@ describe('auth', () => {
   });
 
   describe('signup', () => {
-    it('should create a user in the database', async () => {});
-    it('should return the user information', async () => {});
-    it('should log a user in once the account is created', async () => {});
-    it('should create an http-only cookie that expires in 10 days', async () => {});
-    it('should return 400 with an error when a user already exists', async () => {});
+    let expiration: number | undefined;
+    let db: typeof mongoose | undefined;
+
+    beforeEach(async () => {
+      db = await mongoose.connect(DB_URI);
+      expiration = Date.now() + 864000000;
+    });
+
+    afterEach(async () => {
+      await UserModel.findOneAndDelete(testUser.auth);
+      await db?.disconnect();
+      db = undefined;
+      expiration = undefined;
+    });
+
+    it('should create a user in the database', async () => {
+      const res = await testApp
+        .post('/api/auth/signup')
+        .set('Content-Type', 'application/json')
+        .send(testUser.auth);
+
+      const user: User | null = await UserModel.findOne({
+        username: testUser.auth.username,
+        email: testUser.auth.email,
+      });
+
+      expect(user).not.toBeNull();
+      expect(strip(user!, ['password']).auth).toEqual(
+        strip(testUser, ['password']).auth
+      );
+      expect(res.status).toBe(200);
+    });
+
+    it('should return the new users information', async () => {
+      const res = await testApp
+        .post('/api/auth/signup')
+        .set('Content-Type', 'application/json')
+        .send(testUser.auth);
+
+      const data: User = res.body;
+
+      expect(strip(data, ['password']).auth).toEqual(
+        strip(testUser, ['password']).auth
+      );
+      expect(data.data).toBeDefined();
+      expect(res.status).toBe(200);
+    });
+
+    it('should create an http-only cookie that expires in 10 days', async () => {
+      const res = await testApp
+        .post('/api/auth/signup')
+        .set('Content-Type', 'application/json')
+        .send(testUser.auth);
+
+      const formattedCookie: {
+        ssid: string;
+        httponly: boolean;
+        expires: string;
+      } = formatCookie(res.headers['set-cookie'][0]);
+      expect(formattedCookie.ssid).toBeDefined();
+      expect(formattedCookie.httponly).toBe(true);
+      expect(
+        new Date(formattedCookie.expires).getMilliseconds()
+      ).toBeGreaterThanOrEqual(expiration!);
+      expect(res.status).toBe(200);
+    });
+
+    it('should return 400 with an error when a user already exists', async () => {
+      await UserModel.create(testUser);
+
+      const res = await testApp
+        .post('/api/auth/signup')
+        .set('Content-Type', 'application/json')
+        .send(testUser.auth);
+
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBeDefined();
+    });
+
+    it('should return 400 and an error when missing details', async () => {
+      const missingUsername = await testApp
+        .post('/api/auth/signup')
+        .set('Content-Type', 'application/json')
+        .send(strip(testUser, ['username']).auth);
+
+      const missingName = await testApp
+        .post('/api/auth/signup')
+        .set('Content-Type', 'application/json')
+        .send(strip(testUser, ['name']).auth);
+
+      const missingEmail = await testApp
+        .post('/api/auth/signup')
+        .set('Content-Type', 'application/json')
+        .send(strip(testUser, ['email']).auth);
+
+      const missingPassword = await testApp
+        .post('/api/auth/signup')
+        .set('Content-Type', 'application/json')
+        .send(strip(testUser, ['password']).auth);
+
+      const missingAvatar = await testApp
+        .post('/api/auth/signup')
+        .set('Content-Type', 'application/json')
+        .send(strip(testUser, ['avatar']).auth);
+
+      const none = await testApp
+        .post('/api/auth/signup')
+        .set('Content-Type', 'application/json')
+        .send({});
+
+      expect(missingUsername.status).toBe(400);
+      expect(missingUsername.body.error).toBeDefined();
+
+      expect(missingName.status).toBe(400);
+      expect(missingName.body.error).toBeDefined();
+
+      expect(missingEmail.status).toBe(400);
+      expect(missingEmail.body.error).toBeDefined();
+
+      expect(missingPassword.status).toBe(400);
+      expect(missingPassword.body.error).toBeDefined();
+
+      expect(missingAvatar.status).toBe(400);
+      expect(missingAvatar.body.error).toBeDefined();
+
+      expect(none.status).toBe(400);
+      expect(none.body.error).toBeDefined();
+    });
   });
 
   describe('get', () => {
-    it('should return the user info', async () => {});
-    it('should return 401 and an error when the user is not logged in', async () => {});
+    it('should return the user info', async () => {
+      const res = await testApp
+        .post('/api/auth/login')
+        .set('Content-Type', 'application/json')
+        .send({
+          username: testUser.auth.username,
+          password: testUser.auth.password,
+        });
+
+      const {
+        ssid,
+      }: {
+        ssid: string;
+        httponly: boolean;
+        expires: string;
+      } = formatCookie(res.headers['set-cookie'][0]);
+
+      const { status, body } = await testApp
+        .get('/api/user')
+        .set('Cookie', `SSID=${ssid}`);
+
+      expect(status).toBe(200);
+      expect(body).toEqual(strip(testUser, ['password']));
+    });
+
+    it('should return 401 and an error when the user is not logged in', async () => {
+      const res = await testApp
+        .post('/api/auth/login')
+        .set('Content-Type', 'application/json')
+        .send({
+          username: testUser.auth.username,
+          password: testUser.auth.password,
+        });
+
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBeDefined();
+    });
   });
 });
