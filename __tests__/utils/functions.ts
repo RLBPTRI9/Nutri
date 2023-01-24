f6e0913b473b45d69e5d28810843346451e8c254
Mathew Hultquist <mathew@gates.services>
2023-01-24 13:56:14 -0500

    Added strip function

--- __tests__/utils/functions.ts
+++ __tests__/utils/functions.ts
@@ -2,6 +2,8 @@
 // It turns this: ' Set-Cookie: SSID=1234; HttpOnly; Expires='some date string' '
 // into this: { ssid: '1234', httponly: true, expires: 'some date string' }
 
+import { User } from '../../backend/types/User';
+
 export const formatCookie = (a: string): any =>
   a
     // Remove 'Set-Cookie' header text
@@ -24,3 +26,17 @@ export const formatCookie = (a: string): any =>
       }),
       {}
     );
+
+// This will strip info from a user object
+export const strip = (user: User, stripMe: string[]): any => {
+  const noInfoAuth = Object.entries(user.auth).filter(
+    (e) => !stripMe.includes(e[0])
+  );
+  const noInfoData = Object.entries(user.data).filter(
+    (e) => !stripMe.includes(e[0])
+  );
+  return {
+    auth: Object.fromEntries(noInfoAuth),
+    data: Object.fromEntries(noInfoData),
+  };
+};
