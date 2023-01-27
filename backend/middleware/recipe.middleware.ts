cf3bd4c53120423c2ae5535aca3937057df6c524
Mathew Hultquist <mathew@gates.services>
2023-01-26 20:29:51 -0500

    Re-Added recipe search

--- backend/middleware/recipe.middleware.ts
+++ backend/middleware/recipe.middleware.ts
@@ -7,8 +7,29 @@ interface RecipeInterface {
 
 const recipeMiddleware: RecipeInterface = {
   search: (req: Request, res: Response, next: NextFunction) => {
-    return next();
+    const { q } = req.query;
+    // console.log('what is q' + q);
+    fetch(
+      `https://api.edamam.com/search?app_id=${process.env.EDAMAM_RECIPE_API_ID}&app_key=${process.env.EDAMAM_RECIPE_API_KEY}&q=${q}`
+    )
+      .then((data) => data.json())
+      .then((data) => {
+        if (!data) {
+          res.locals.recipeData = null;
+        } else {
+          const recipeData = data;
+
+          // TODO: change recipe data to be same format as type
+
+          res.locals.recipeData = recipeData;
+          return next();
+        }
+      })
+      .catch((err) => {
+        return next(err);
+      });
   },
+
   get: (req: Request, res: Response, next: NextFunction) => {
     return next();
   },
