77f2379eff12b513fe20e800ea4ad380bdcd8876
Mathew Hultquist <mathew@gates.services>
2023-01-28 11:01:34 -0500

    Prettier

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client';
 import App from './components/App';
 import { NutriContextProvider } from './store/nutri-context.js';
 import { Provider } from 'react-redux';
-import { store } from './store/store'
+import { store } from './store/store';
 
 createRoot(document.querySelector('#app')!).render(
   <React.StrictMode>
     <Provider store={store}>
-    <NutriContextProvider>
-      <App />
-    </NutriContextProvider>
+      <NutriContextProvider>
+        <App />
+      </NutriContextProvider>
     </Provider>
   </React.StrictMode>
 );
