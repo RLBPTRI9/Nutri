cecf99b8912759f99b61296a4e7d20c468b769ff
Mathew Hultquist <mathew@gates.services>
2023-01-28 11:02:29 -0500

    Began adding favorites page

--- frontend/components/Favorites.tsx
+++ frontend/components/Favorites.tsx
@@ -1,16 +1,73 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { UserState } from '../features/userSlice'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import { AuthState } from '../features/authSlice';
+import { Recipe } from '../features/interfaces/recipe';
+import { UserState } from '../features/userSlice';
+import RecipeCard from './RecipeCard';
+import { store } from '../store/store';
 
 const Favorites = () => {
+  const favorites: Recipe[] = useSelector<UserState, Recipe[]>(
+    (state) => state.favorites
+  );
+  const auth: AuthState = useSelector<AuthState, AuthState>((state) => state);
 
-    const favorites = useSelector<UserState>((state) => state.favorites)
+  //   const store1 = store.getState();
 
-    return (
-        <div>helloooo favorites page</div>
-    //render favorites below using favorites variable above
+  //   console.log(store1);
+  console.log(auth);
+  //   console.log(favorites);
+  // if (!auth.isLoggedIn) return Navigate({ to: '/login' });
 
-    )
-}
+  return (
+    <div className='Favorites'>
+      {/* {auth.username ? ( */}
+      <div className='Favs'>
+        {favorites?.length ? (
+          favorites.map((recipe) => (
+            <RecipeCard
+              key={recipe.recipeId}
+              name={recipe.recipeName}
+              image={recipe.image}
+              url={recipe.sourceUrl}
+              source={recipe.sourceUrl}
+              ingredients={recipe.ingredients}
+            />
+          ))
+        ) : (
+          <h1>No favs</h1>
+        )}
+      </div>
+      {/* ) : (
+        <h1>Not logged in</h1>
+      )} */}
+      {/* <div className='Favs'>
+        {favorites?.length ? (
+          favorites.map((recipe) => (
+            <RecipeCard
+              key={recipe.recipeId}
+              name={recipe.recipeName}
+              image={recipe.image}
+              url={recipe.sourceUrl}
+              source={recipe.sourceUrl}
+              ingredients={recipe.ingredients}
+            />
+          ))
+        ) : (
+          <h1>No favs</h1>
+        )}
+      </div> */}
+    </div>
+  );
+};
 
 export default Favorites;
+
+//   name?: string;
+//   image?: string;
+//   healthLabels?: string[];
+//   url?: string;
+//   ingredients?: any;
+//   source?: string;
+//   props?: any;
